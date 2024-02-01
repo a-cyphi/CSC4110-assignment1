b8bb3f34cc7c8a966736413ea1c540702164f894
a-cyphi <acyoherw@gmail.com>
2024-02-01 16:31:49 -0500

    adding author name

--- HelloWorld-1.cpp
+++ HelloWorld-1.cpp
@@ -1,4 +1,6 @@
-// Simple C++ program to display "Hello World" 
+// Simple C++ program to display "Hello World"
+// Author: Audi Woodside <acyoherw@gmail.com>
+ 
 // Header file for input output functions 
 #include<iostream>  
   
